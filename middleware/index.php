82a07c54791841ba8bd012ff46369c7aa32d1206
David Maene <davidmened@gmail.com>
2022-07-01 19:37:01 +0200

    --

--- middleware/index.php
+++ middleware/index.php
@@ -2,7 +2,7 @@
 date_default_timezone_set('UTC');
 require_once("pont.php"); // don't delete or modify this line
 // ----------------------------------------------------------
-// ----------------  include classes here ---------------------
+// ----------------  include classes here -------------------
 // ----------------------------------------------------------
 include_once("models/cl.user.php");
 include_once("models/cl.rubriques.php");
@@ -28,43 +28,45 @@ function _fillPhoneNumber($string){
 
 if($_GET['curl']){
     $curl = $_GET['curl'];
-   switch ($curl) {
+    switch ($curl) {
 
-    case 'connexion':
-        $admin = new Admins();
-        $admin = $admin->getOne(
-                array(
-                "phone" => _fillPhoneNumber($_POST['phone']),
-                "password" => md5($_POST['password'])
-            )
-        );
+        case 'connexion':
+            $admin = new Admins();
+            $admin = $admin->getOne(
+                    array(
+                    "phone" => _fillPhoneNumber($_POST['phone']),
+                    "password" => md5($_POST['password'])
+                )
+            );
 
-        try {
-            $a = (array) $admin->body;
-            if($admin->status === 200){
-                if(1 && count($a) > 0){
-                    $_SESSION['_bigUser'] = base64_encode(json_encode($a));
-                    $_SESSION['token'] = base64_encode($a['id']);
-                    echo($admin->print());
-                } else {
-                    $res = new Response(404, "No Item found !");
+            try {
+                $a = (array) $admin->body;
+                if($admin->status === 200){
+                    if(1 && count($a) > 0){
+                        $_SESSION['_bigUser'] = base64_encode(json_encode($a));
+                        $_SESSION['token'] = base64_encode($a['id']);
+                        echo($admin->print());
+                    } else {
+                        $res = new Response(404, "No Item found !");
+                        echo($res->print());
+                    }
+                }else{
+                    $res = new Response(500, "Something went wrong !");
                     echo($res->print());
                 }
-            }else{
-                $res = new Response(500, "Something went wrong !");
+            } catch (\Throwable $th) {
+                $res = new Response(500, $th);
                 echo($res->print());
             }
-        } catch (\Throwable $th) {
-            $res = new Response(500, $th);
+            break;
+        case '':
+            break;
+        
+        default:
+            $res = new Response(404, "Aucune route trouvée avec comme clé ");
             echo($res->print());
-        }
-        break;
-    
-    default:
-        $res = new Response(404, "Aucune route trouvée avec comme clé ");
-        echo($res->print());
-        break;
-   }
+            break;
+    }
 
 }else{
     $res = new Response(404, "Aucune route trouvée avec comme clé ");
