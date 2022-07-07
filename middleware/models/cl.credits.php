afcf6b4cb98a1558a6ed877645dee89d0022a1b5
David Maene <davidmened@gmail.com>
2022-07-07 16:35:10 +0200

    up me line

---
+++ middleware/models/cl.credits.php
@@ -0,0 +1,25 @@
+<?php 
+class Credits extends CRUD__{
+
+    public $id;
+    public $idaccount;
+    public $montantdu;
+    public $montantpaye;
+    public $devise;
+    public $createdon;
+    public $updatedon;
+    public $status;
+
+    public function __constructor($id, $idaccount, $montantdu, $montantpaye, $devise, $createdon, $updatedon, $status){
+        $this->id = $id;
+        $this->idaccount = $idaccount;
+        $this->montantdu = $montantdu;
+        $this->montantpaye = $montantpaye;
+        $this->devise = $devise;
+        $this->createdon = $createdon;
+        $this->updatedon = $updatedon;
+        $this->status = $status;
+    }   
+}
+
+?>
